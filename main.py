Fixed error in reflectivity calculation: Matrix not correctly initialised.

main.py
@@ -17,6 +17,7 @@ def _make_matrix(M: np.int_, n: np.ndarray, d: np.ndarray, k_outer: np.float_, n
     def calc_k_x(n):
         return k_outer * np.sqrt((n / n_outer) ** 2 - np.sin(theta_outer) ** 2)
 
+    k_x_outer: np.ndarray = k_outer * np.cos(theta_outer)
     k_x: np.ndarray = calc_k_x(n)
     k_x_substrate: np.float_ = calc_k_x(n_substrate)
     phi: np.ndarray = k_x * d
@@ -27,8 +28,10 @@ def _make_matrix(M: np.int_, n: np.ndarray, d: np.ndarray, k_outer: np.float_, n
     ]
 
     betas: list[np.ndarray] = [
+        np.array([[1, np.exp(1j * phi[0])],[k_x[0] / k_x_outer, -k_x[0] / k_x_outer * np.exp(1j * phi[0])]])
+    ] + [
         np.array([[1, np.exp(1j * phi[j])], [k_x[j], -k_x[j] * np.exp(1j * phi[j])]])
-        for j in range(0, M)
+        for j in range(1, M)
     ]
 
     columns: list[np.ndarray] = [

unit_tests/tests_reflectivity/test_reflectivity.py
@@ -8,13 +8,21 @@ from main import reflectivity_s
 
 
 class Test_reflectivity(TestCase):
+
+    @classmethod
+    def _make_test_matrix_for_one_layer(cls, M: np.int_, n: np.ndarray, d: np.ndarray, k_outer: np.float_, n_outer: np.float_, n_substrate: np.float_,
+                 theta_outer: np.float_) -> np.ndarray:
+        return
+
+
+
     def test_thin_film_interference(self):
         M: int = 1
-        d: np.ndarray = np.array([50]) * 1e-9
+        d: np.ndarray = np.array([1]) * 1e-3
         n_outer = 1.00
-        n_layer = 2.0
+        n_layer = 1.50
         n: np.ndarray = np.array([n_layer])
-        n_substrate = 1.00
+        n_substrate = n_outer
         theta_outer = 0.
 
         # Fresnel coefficients
@@ -47,19 +55,37 @@ class Test_reflectivity(TestCase):
         r_23: np.complex_ = r_ij(n_layer, cos_theta_layer, n_substrate, cos_theta_substrate)
         t_23: np.complex_ = t_ij(n_layer, cos_theta_layer, n_substrate, cos_theta_substrate)
 
-        self.assertAlmostEqual(r_12.real, -r_23.real, places=7)
-        self.assertAlmostEqual(r_12.imag, -r_23.imag, places=7)
+        self.assertAlmostEqual(r_12.real, -r_23.real, places=15)
+        self.assertAlmostEqual(r_12.imag, -r_23.imag, places=15)
 
         def reflection_coefficient(r, lambda_0, n, d, cos_theta):
             phi = 2 * np.pi / lambda_0 * n * d * cos_theta
             return 4 * (r ** 2) * (np.sin(phi) ** 2) / ((1 - r ** 2) ** 2 + 4 * (r ** 2) * (np.sin(phi) ** 2))
 
+        wavelengths = np.linspace(1, 10, num=10000) * 1e-6
+        R_correct = np.array([reflection_coefficient(r_12, lam, n_layer, d[0], cos_theta_layer) for lam in wavelengths])
+        R_to_be_tested = np.array([reflectivity_s(M, n, d, lam, n_outer, n_substrate, theta_outer) for lam in wavelengths])
+
+        fig: plt.Figure
+        ax: plt.Axes
+        fig, ax = plt.subplots()
+        ax.plot(wavelengths, R_correct)
+        ax.plot(wavelengths, R_to_be_tested)
+        ax.legend()
+        # ax.set_ylim(0,1.1)
+        fig.show()
+
+        self.assertEqual(R_correct.shape[0], R_to_be_tested.shape[0])
+        for correct, to_test in zip(R_correct, R_to_be_tested):
+            self.assertAlmostEqual(correct, to_test, places=15)
+        # theta = np.linspace(0,0.06,num=1000)
+        # R1 = np.array([reflection_coefficient(np.sqrt(0.5), 400* 1e-9, n_layer, d[0], np.cos(t)) for t in theta])
+        # R2 = np.array([reflection_coefficient(np.sqrt(0.7), 400* 1e-9, n_layer, d[0], np.cos(t)) for t in theta])
+        # R3 = np.array([reflection_coefficient(np.sqrt(0.9), 400* 1e-9, n_layer, d[0], np.cos(t)) for t in theta])
 
 
 
-
-
-    def test_reflectivity_s(self):
+    def test_antireflection_coating(self):
         df = pd.read_csv('./data/Hobson_Baldwin_2004_anti_reflection_coating_data.txt')
 
         M: int = df.shape[0]
@@ -82,9 +108,8 @@ class Test_reflectivity(TestCase):
         )
         for lam in wavelengths])
 
-        print(R[0])
-
         fig, ax = plt.subplots()
+        # figsize=(12, 9), dpi=600
         ax: plt.Axes
         ax.plot(wavelengths * 1e9, R)
         ax.set_xlabel(r'$\lambda_\mathrm{outer}$ [nm]')
@@ -92,4 +117,34 @@ class Test_reflectivity(TestCase):
         # ax.set_ylim(0, 1.2)
         plt.show()
 
+    def test_dichroic(self):
+        df = pd.read_csv('./data/Hobson_Baldwin_2004_dichroic_data.txt')
 
+        M: int = df.shape[0]
+        n: np.ndarray = df.iloc[:, 1].values
+        print(df.iloc[:, 2].values, type())
+        d: np.ndarray = df.iloc[:, 2].values * 1e-9
+        n_outer = 1.00
+        n_substrate = 1.50
+        theta_outer = 0.
+
+        wavelengths = np.linspace(600, 2300, 1000) * 1e-9
+        R = np.array([reflectivity_s(
+            M,
+            n,
+            d,
+            lam,
+            n_outer,
+            n_substrate,
+            theta_outer
+        )
+        for lam in wavelengths])
+
+        fig, ax = plt.subplots()
+        # figsize=(12, 9), dpi=600
+        ax: plt.Axes
+        ax.plot(wavelengths * 1e9, R)
+        ax.set_xlabel(r'$\lambda_\mathrm{outer}$ [nm]')
+        ax.set_ylabel('R')
+        # ax.set_ylim(0, 1.2)
+        plt.show()