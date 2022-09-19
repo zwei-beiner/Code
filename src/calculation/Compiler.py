c7cf1b2d4fec10b1d94bcfe2d7dee55287af95c2
zweibeiner <nk544@cam.ac.uk>
2022-09-19 14:26:40 +0100

    Added comments.

--- src/calculation/Compiler.py
+++ src/calculation/Compiler.py
@@ -2,9 +2,15 @@ import subprocess
 from pathlib import Path
 
 
-def compile_cython_files():
-    # Compile Cython .pyx files
+def compile_cython_files() -> None:
+    """
+    Compiles cython '.pyx' files by calling the command 'python setup.py build_ext --inplace'. The '--inplace' command
+    option places the generated '.so' files into the same folder as the cython '.pyx' files.
+    """
+
     path = Path(__file__).parents[1] / 'setup.py'
     cmd = f'python {str(path)} build_ext --inplace'
+    # Run command from command line.
     p = subprocess.run([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
+    # Redirect stdout and stderr to stdout and print to console.
     print(p.stdout)
