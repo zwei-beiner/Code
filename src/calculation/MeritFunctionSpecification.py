5c04130d8b4583c5348b39c604ef6e92897e14d1
zweibeiner <nk544@cam.ac.uk>
2022-09-19 15:34:20 +0100

    Added comments.

--- src/calculation/MeritFunctionSpecification.py
+++ src/calculation/MeritFunctionSpecification.py
@@ -2,6 +2,22 @@ import numpy as np
 from typing import Callable
 
 class MeritFunctionSpecification:
+    """
+    Class which stores all the variables which appear in the merit function.
+
+    The merit function is a function of the refractive indices of each layer, the thicknesses of each layer, and a set
+    of wavelengths. Its functional form is:
+
+          (s_pol_weighting) * ((R_s - target_reflectivity_s) / weight_function_s)^2
+        + (p_pol_weighting) * ((R_p - target_reflectivity_p) / weight_function_p)^2
+        + (sum_weighting) * (((R_s + R_p) - target_sum) / weight_function_sum)^2
+        + (difference_weighting) * ((|R_s - R_p| - target_difference) / weight_function_difference)^2
+        + (phase_weighting) * ((phase_difference - target_relative_phase) / weight_function_phase)^2
+
+    where 'R_s' and 'R_p' are the reflectivities for s- and p-polarised light and 'phase_difference' is the phase
+    difference between the amplitudes of s- and p-polarised light.
+    """
+
     def __init__(self,
                  p_pol_weighting: float,
                  s_pol_weighting: float,
--- src/calculation/__init__.py
+++ src/calculation/__init__.py
@@ -2,9 +2,7 @@ import sys
 from pathlib import Path
 
 # Add subdirectory manually to sys.path. This is necessary because we can't place an __init__.py file into
-# the subdirectory, as this breaks Cython (this is a known Cython bug)
-# This enables us to import modules from the subdirectory directly, e.g. 'import reflectivity_c_file'
-# sys.path.insert(1, str(Path(__file__).parent / 'calculation'))
-# sys.path.insert(1, str(Path(__file__).parent / 'calculation' / 'cython_files'))
+# the 'cython_files' subdirectory, as this breaks Cython (this is a known Cython bug).
+# This enables us to import modules from the 'cython_files' subdirectory directly, e.g. 'import reflectivity_c_file'.
 sys.path.insert(1, str(Path(__file__).parent))
 sys.path.insert(1, str(Path(__file__).parent / 'cython_files'))
